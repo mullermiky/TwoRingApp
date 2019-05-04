5ac2cd6e5b6aaf4690d5b38f7122d47b51389055
Michael Müller <muller.miky@gmail.com>
2019-05-04 20:08:14 +0200

    Delete SQLQuery3.sql

--- sql/SQLQuery3.sql
+++
@@ -1,9 +0,0 @@
-﻿/****** Script for SelectTopNRows command from SSMS  ******/
-
-declare @tmp as [RecordIdList]
-
-	insert into @tmp 
-	select 60
-
-
-exec dbo.ComputeData @from = '2017-12-31', @to = '2019-05-02', @queues = @tmp
