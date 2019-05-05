22587dd1a3b6d7619ab75ecea1ff2dc0c1fceb2b
Michael Müller <muller.miky@gmail.com>
2019-05-05 09:53:08 +0200

    Updated date inputs check

--- client/scripts/ShowData.js
+++ client/scripts/ShowData.js
@@ -43,92 +43,103 @@ $(document).ready(function(){
         let dateTimeFrom = document.getElementById("fromDateTime").value;
         let dateTimeTo = document.getElementById("toDateTime").value;
         let queues = document.getElementById("queues").value;
+        var compare_dates = function(date1,date2){
+            if (date1>date2) return (false);
+            else if (date1<date2) return (true);
+            else return (false); 
+        }
+
+        console.log(compare_dates(dateTimeFrom, dateTimeTo));
+        
 
         if (dateTimeFrom && dateTimeTo){
-            
-            
-            let splitQueues = queues.split(",");
-            console.log(queues);
-            console.log(splitQueues);
-            if (!splitQueues) {
-                splitQueues = queues;
-            }
-
-            let url = 'https://localhost:44394/api/Data?from='+dateTimeFrom+'&to='+dateTimeTo;
-            splitQueues.map(queue => {
-                url = url + '&ids='+queue;
-            });
-            
-            const param = {
-                method: "GET",
-                mode: "cors",
-                cache: "no-cache",
-                credentials: "same-origin",
-                headers:{
-                    "content-type":"application/json; charset=UTF-8"
+            if (compare_dates(dateTimeFrom, dateTimeTo)){
+                let splitQueues = queues.split(",");
+                
+                if (!splitQueues) {
+                    splitQueues = queues;
                 }
-            }
 
-            fetch(url, param)
-                .then(function(response) {
-                    if (!response.ok) {
-                        throw Error(response.statusText);
-                    }
-                    return response;
-                })
-                .then(data=>{return data.json()})
-                .then(res=>{
-                    res.map(r => {
-                        console.log(r);
-
-                        let container = document.getElementById('tableBody');
-
-                        const row = document.createElement('tr');
-
-                        const column = document.createElement('th');
-                        column.setAttribute('scope', 'row');
-                        column.innerHTML = r.csqName;
-
-                        const column1 = document.createElement('td');
-                        column1.setAttribute('scope', 'row');
-                        column1.innerHTML = r.loggedinagents;
-
-                        const column2 = document.createElement('td');
-                        column2.setAttribute('scope', 'row');
-                        column2.innerHTML = r.availableagents;
-
-                        const column3 = document.createElement('td');
-                        column3.setAttribute('scope', 'row');
-                        column3.innerHTML = r.numberOfCalls;
-
-                        const column4 = document.createElement('td');
-                        column4.setAttribute('scope', 'row');
-                        column4.innerHTML = r.numberOfhandledCalls;
-
-                        const column5 = document.createElement('td');
-                        column5.setAttribute('scope', 'row');
-                        column5.innerHTML = r.numberOfAbandonedCalls;
-
-                        row.appendChild(column);
-                        row.appendChild(column1);
-                        row.appendChild(column2);
-                        row.appendChild(column3);
-                        row.appendChild(column4);
-                        row.appendChild(column5);
-
-                        container.append(row);
-
-                    })
-                    
-                })
-                .catch(function(error) {
-                    console.warn(error);
-                    document.getElementById('toastErrorBody').innerHTML = error;
-                    $('#errorToast').toast('show');
-                    
+                let url = 'https://localhost:44394/api/Data?from='+dateTimeFrom+'&to='+dateTimeTo;
+                splitQueues.map(queue => {
+                    url = url + '&ids='+queue;
                 });
-        }else{
+                
+                const param = {
+                    method: "GET",
+                    mode: "cors",
+                    cache: "no-cache",
+                    credentials: "same-origin",
+                    headers:{
+                        "content-type":"application/json; charset=UTF-8"
+                    }
+                }
 
+                fetch(url, param)
+                    .then(function(response) {
+                        if (!response.ok) {
+                            throw Error(response.statusText);
+                        }
+                        return response;
+                    })
+                    .then(data=>{return data.json()})
+                    .then(res=>{
+                        res.map(r => {
+                            //console.log(r);
+
+                            let container = document.getElementById('tableBody');
+
+                            const row = document.createElement('tr');
+
+                            const column = document.createElement('th');
+                            column.setAttribute('scope', 'row');
+                            column.innerHTML = r.csqName;
+
+                            const column1 = document.createElement('td');
+                            column1.setAttribute('scope', 'row');
+                            column1.innerHTML = r.loggedinagents;
+
+                            const column2 = document.createElement('td');
+                            column2.setAttribute('scope', 'row');
+                            column2.innerHTML = r.availableagents;
+
+                            const column3 = document.createElement('td');
+                            column3.setAttribute('scope', 'row');
+                            column3.innerHTML = r.numberOfCalls;
+
+                            const column4 = document.createElement('td');
+                            column4.setAttribute('scope', 'row');
+                            column4.innerHTML = r.numberOfhandledCalls;
+
+                            const column5 = document.createElement('td');
+                            column5.setAttribute('scope', 'row');
+                            column5.innerHTML = r.numberOfAbandonedCalls;
+
+                            row.appendChild(column);
+                            row.appendChild(column1);
+                            row.appendChild(column2);
+                            row.appendChild(column3);
+                            row.appendChild(column4);
+                            row.appendChild(column5);
+
+                            container.append(row);
+
+                        })
+                        
+                    })
+                    .catch(function(error) {
+                        console.warn(error);
+                        document.getElementById('toastErrorBody').innerHTML = error;
+                        $('#errorToast').toast('show');
+                        
+                });
+            }else{
+                document.getElementById('toastErrorBody').innerHTML = "To must be bigger than from";
+                $('#errorToast').toast('show');
+            }
+        }else{
+            document.getElementById('toastErrorBody').innerHTML = "Fill date inputs";
+            $('#errorToast').toast('show');
         }
 
     }
